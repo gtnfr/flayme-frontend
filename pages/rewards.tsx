46e89454b0ddafced6183e87366301fd36cbaa0e
GTNFT <72807614+gtnfr@users.noreply.github.com>
2025-07-25 22:29:40 +0100

    Update rewards.tsx

---
+++ pages/rewards.tsx
@@ -0,0 +1,10 @@
+export default function Rewards() {
+  return (
+    <main className="min-h-screen flex flex-col items-center justify-center bg-black text-white">
+      <h1 className="font-poppins text-4xl mb-4">Rebase Rewards</h1>
+      <div className="glass p-6 rounded-2xl">
+        <p>Rebase payout dates coming soon! <br /> APY Calculator will appear here.</p>
+      </div>
+    </main>
+  );
+}
